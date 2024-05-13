4aebe07b2e585a97c58354051e6456501144b2e3
shan chu <shanchu@shans-mbp.mynetworksettings.com>
2024-05-13 15:15:33 -0400

    homepage search item

--- findmine-course/src/app/home/home.component.ts
+++ findmine-course/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FoodService} from "../services/food/food.service";
 import {Food} from "../shared/Food";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,16 @@ import {Food} from "../shared/Food";
 })
 export class HomeComponent implements OnInit {
 foods:Food[] = [];
-  constructor(private foodService:FoodService) { }
+  constructor(private foodService:FoodService,private route:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.foods = this.foodService.getAll();
+    this.route.params.subscribe(params =>{
+      if(params.searchTerm){
+        this.foods = this.foodService.getAll().filter(food=>food.name.toLowerCase().includes(params.searchTerm.toLowerCase()))
+      }else{
+        this.foods = this.foodService.getAll();
+      }
+    })
   }
 
 }
